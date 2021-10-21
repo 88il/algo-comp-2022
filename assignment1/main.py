1433deb1bbfaef3902f6ac06e84196138066d300
Iris Lang <pl235091@ahschool.com>
2021-10-21 11:30:26 -0400

    ain.py

--- assignment1/main.py
+++ assignment1/main.py
@@ -2,6 +2,8 @@
 import json
 import sys
 import os
+from operator import itemgetter
+from collections import Counter
 
 INPUT_FILE = 'testdata.json' # Constant variables are usually in ALL CAPS
 
@@ -16,8 +18,59 @@ class User:
 
 # Takes in two user objects and outputs a float denoting compatibility
 def compute_score(user1, user2):
-    # YOUR CODE HERE
-    return 0
+    # Initialize compatibility to 0
+    compatibility = 0
+    num_questions = len(users[0].responses)
+    num_answer_choices = 5
+               
+               
+    # WEIGHT for gradYear compatibility
+    year1 = user1.grad_year
+    year2 = user2.grad_year
+    # Smaller age gap results in larger gradYear_compatibility
+    # Age gap of 0 corresponds to gradYear_compatibility of 1/4... age gap of 4 deducts 1/12 from overall compatibility
+    gradYear_compatibility = (3 - abs(year1 - year2)) / 12
+    compatibility += gradYear_compatibility
+    
+    
+    # WEIGHT for gender preferences–only add points if preferences agree mutually
+    if (user1.preferences[0] == user2.gender and user2.preferences[0] == user1.gender):
+        compatibility += 1 / 12
+    
+    
+    # WEIGHT responses based on frequency response is chosen
+    # List of lists of length num_questions for response weights based on frequency of response
+    response_weights = [[]] * num_questions
+    
+    for i in range(num_questions):
+        # List of responses to question i
+        responses_i = []
+        for j in range(len(users)):
+            # List of responses to question i
+            responses_i.append(users[j].responses[i])
+        for j in range(num_answer_choices):
+            # Frequency of answer j for question i
+            freq = Counter(responses_i)[j]
+            # Weight is max 1, min 20/(20+12) = 5/8
+            weight = num_questions / (num_questions + freq)
+            response_weights[i].append(weight)
+            
+    # Calculate response weights between two users
+    sum_weights = 0
+    for i in range(num_questions):
+        if (user1.responses[i] == user2.responses[i]):
+            # If same answer, get corresponding response weight
+            sum_weights += response_weights[i][user1.responses[i]]
+
+
+    # Normalize sum to (0,1)
+    compatibility += sum_weights / 20
+    
+    
+    # Max compatibility is 1 + 1/12 + 1/12 = 7 / 6. Normalize:
+    compatibilitiy = 6 * compatibility / 7
+    
+    return compatibility
 
 
 if __name__ == '__main__':
